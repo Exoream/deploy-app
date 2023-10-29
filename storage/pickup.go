5fd1d9daee4c8a62265f55677cd71504f5bddb59
Al Hilaluddin <shiigetsu1@gmail.com>
2023-10-29 17:50:04 +0800

    bismillah

--- storage/pickup.go
+++ storage/pickup.go
@@ -4,42 +4,29 @@ import (
 	"context"
 	"encoding/base64"
 	"io"
-	"io/ioutil"
 	"mime/multipart"
 	"os"
 
 	"cloud.google.com/go/storage"
 	"github.com/google/uuid"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/api/option"
 )
 
 func UploadImageForPickup(image *multipart.FileHeader) (string, error) {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		logrus.Error("Config : Cannot load config file,", err.Error())
-	}
-
 	ctx := context.Background()
 
-	// keys credentials google cloud
-	credentialBase64 := os.Getenv("GOOGLE_CLOUD_CREDENTIALS_BASE64")
-	credentialBytes, err := base64.StdEncoding.DecodeString(credentialBase64)
+	// Decode Google Cloud credentials from Base64
+	encodedCredentials := os.Getenv("GOOGLE_CLOUD_CREDENTIALS_PATH")
+	decodedCredentials, err := base64.StdEncoding.DecodeString(encodedCredentials)
 	if err != nil {
+		logrus.Error("Failed to decode Google Cloud credentials:", err)
 		return "", err
 	}
 
-	credentialFile := "keys.json"
-	err = ioutil.WriteFile(credentialFile, credentialBytes, 0644)
+	client, err := storage.NewClient(ctx, option.WithCredentialsJSON(decodedCredentials))
 	if err != nil {
-		return "", err
-	}
-	defer os.Remove(credentialFile)
-
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(credentialFile))
-	if err != nil {
+		logrus.Error("Failed to create GCP client:", err)
 		return "", err
 	}
 	defer client.Close()
@@ -49,11 +36,15 @@ func UploadImageForPickup(image *multipart.FileHeader) (string, error) {
 
 	wc := client.Bucket(bucketName).Object(imagePath).NewWriter(ctx)
 	defer wc.Close()
+
 	file, err := image.Open()
 	if err != nil {
+		logrus.Error("Failed to open image file:", err)
 		return "", err
 	}
+
 	if _, err := io.Copy(wc, file); err != nil {
+		logrus.Error("Failed to copy image data:", err)
 		return "", err
 	}
 
